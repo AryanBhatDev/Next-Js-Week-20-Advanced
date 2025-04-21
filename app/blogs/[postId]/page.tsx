cc05b9262180ac6b8d41b563949495035273cfa2
Aryan Bhat <aryanbhat2527@gmail.com>
2025-04-21 20:44:35 +0530

    how to get the dynamic patams

---
+++ app/blogs/[postId]/page.tsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+
+export default async function Blog({params}:{params:{
+    postId: number
+}}){
+    const postId = (await params).postId;
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    const data = res.data;
+    return <div>
+        Blog page {postId}
+
+        <br/>
+        title - {data.title}
+        body - {data.body}
+
+    </div>
+}
